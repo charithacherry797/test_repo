c57d06ff5c51a54542b8061ae9890f92ba97469f
tamatam <charitha.tamatam@capgemini.com>
2019-03-04 12:32:08 +0530

    modified Calculator.java file

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator{
         public static void main(String []args){
               System.out.println("Hello world!!!!");
+              System.out.println("calculator java program");
          }
 }
